457d4ce702c0a3bbff0003ec009b871644c99e08
randy41010-commits <randy41010@gmail.com>
2026-08-11 20:41:58 +0800

    Refine contact information layout

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -6,13 +6,11 @@ export default function KontaktPage() {
       <SiteHeader />
       <PageIntro index="05" title="Kontakt" text="For commissions, exhibitions, collaborations or simply an exchange of ideas." />
       <section className="kontakt-main">
-        <p>Let’s make<br />something <em>clear.</em></p>
-        <a className="mail-link" href="mailto:Randy41010@gmail.com">Randy41010@gmail.com <span>↗</span></a>
-      </section>
-      <section className="kontakt-grid">
-        <div><span>Phone</span><a href="tel:+4915224046270">+49 152 24046270</a></div>
-        <div><span>Academic email</span><a href="mailto:haoran.wang@rsh-duesseldorf.de">haoran.wang@rsh-duesseldorf.de</a></div>
-        <div><span>Availability</span><p>Selected commissions<br />from September 2026</p></div>
+        <div className="contact-links">
+          <a className="contact-link" href="mailto:Randy41010@gmail.com"><span>Personal email</span>Randy41010@gmail.com <i>↗</i></a>
+          <a className="contact-link" href="mailto:haoran.wang@rsh-duesseldorf.de"><span>Academic email</span>haoran.wang@rsh-duesseldorf.de <i>↗</i></a>
+          <a className="contact-link" href="tel:+4915224046270"><span>Phone</span>+49 152 24046270 <i>↗</i></a>
+        </div>
       </section>
       <SiteFooter />
     </main>
